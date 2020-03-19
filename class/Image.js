d35c7ccc0e5e1ce36d1acd81e5fe283bb680bb1d
magol28 <magol@hotmail.es>
2020-03-19 11:53:12 -0500

    database

--- class/Image.js
+++ class/Image.js
@@ -1,11 +1,18 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'root', {
-    host: 'localhost',
-    dialect: 'postgres' 
-  });
+//  sequelize = new Sequelize('postgres', 'postgres', 'root', {
+//     host: 'localhost',
+//     dialect: 'postgres' 
+//   });
   
-
+  const sequelize = new Sequelize(
+    'postgres://poeczbkigfqyci:5af3db3de06f6f0464735b44f73b448a992e4c065f186ed1219bfdec3eee2a2f@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d39huihd2db8j5    ', {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
 
   const Image = sequelize.define("Image", {
     idimage: {
@@ -31,5 +38,6 @@ const sequelize = new Sequelize('postgres', 'postgres', 'root', {
     freezeTableName: true,
     timestamps: true,
 });
+
 
 module.exports = Image;
